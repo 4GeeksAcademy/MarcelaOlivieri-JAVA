e0cbddc5a2cfccb306283d03a53e90996e596c0f
MarOlivieri <141793518+MarOlivieri@users.noreply.github.com>
2024-03-27 16:51:35 +0000

    hecho

--- src/app.js
+++ src/app.js
@@ -6,38 +6,40 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  var excuseGenerator = () => {
-    var who = [
-      "A man",
-      "A lonely jogger",
-      "A lonely comedian",
-      "A lonely merchant",
-      "A slow merchant"
+  let excuseGenerator = () => {
+    let who = [
+      "A man ",
+      "A lonely jogger ",
+      "A lonely comedian ",
+      "A lonely merchant ",
+      "A slow merchant "
     ];
-    var action = ["yelled", "kicked", "burned", "kicked", "threw"];
-    var what = [
-      "at my sandwich",
-      "my sandwich",
-      "my car",
-      "my car",
-      "my computer"
+    let action = ["yelled ", "kicked ", "burned ", "kicked ", "threw "];
+    let what = [
+      "at my sandwich ",
+      "my sandwich ",
+      "my car ",
+      "my car ",
+      "my computer "
     ];
-    var where = [
-      "in my house",
-      "on the street",
-      "front of the office",
-      "near the toilet",
-      "on the street"
+    let where = [
+      "in my house ",
+      "on the street ",
+      "front of the office ",
+      "near the toilet ",
+      "on the street "
     ];
 
-    var whoIndex = who[Math.floor(Math.random() * who.length)];
-    var actionIndex = action[Math.floor(Math.random() * action.length)];
-    var whatIndex = what[Math.floor(Math.random() * what.length)];
-    var whereIndex = where[Math.floor(Math.random() * where.length)];
+    let whoIndex = who[Math.floor(Math.random() * who.length)];
+    let actionIndex = action[Math.floor(Math.random() * action.length)];
+    let whatIndex = what[Math.floor(Math.random() * what.length)];
+    let whereIndex = where[Math.floor(Math.random() * where.length)];
 
-    var excuse = whoIndex + "" + actionIndex + "" + whatIndex + "" + whereIndex;
+    let excuse = whoIndex + "" + actionIndex + "" + whatIndex + "" + whereIndex;
     return excuse;
   };
-  document.querySelector("excuse");
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").style.backgroundColor = "orange";
+  let excuseElement = document.querySelector("#excuse");
+  excuseElement.innerHTML = excuseGenerator();
 };
+console.log("Hello Rigo from the console!");
